bad26198761f8d88dc5c1b4b189acbb5b39080a0
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-09-04 14:16:18 -0600

    Initial commit

---
+++ src/main/java/com/example/leaderboardsystem/LeaderboardsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaderboardsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardsystemApplication.class, args);
+	}
+
+}

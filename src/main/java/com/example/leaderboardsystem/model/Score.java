e15fbd9bb5dec5b8ddab423e6076b3884b36721c
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-09-05 13:06:14 -0600

    Entities

--- src/main/java/com/example/leaderboardsystem/model/Score.java
+++ src/main/java/com/example/leaderboardsystem/model/Score.java
@@ -1,4 +1,32 @@
 package com.example.leaderboardsystem.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Score {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    private String game;
+    private int score;
+
+    @CreationTimestamp
+    private LocalDateTime submittedAt;
+
+    public Score (User user, String game, int score) {
+        this.user = user;
+        this.game = game;
+        this.score = score;
+    }
 }
--- src/main/java/com/example/leaderboardsystem/model/User.java
+++ src/main/java/com/example/leaderboardsystem/model/User.java
@@ -1,4 +1,29 @@
 package com.example.leaderboardsystem.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public User (String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }

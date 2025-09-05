e6cf36d297395adccaff3dc67c407427f720cf60
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-09-05 13:09:55 -0600

    Repositories

--- src/main/java/com/example/leaderboardsystem/repository/ScoreRepository.java
+++ src/main/java/com/example/leaderboardsystem/repository/ScoreRepository.java
@@ -1,4 +1,15 @@
 package com.example.leaderboardsystem.repository;
 
-public class ScoreRepository {
+import com.example.leaderboardsystem.model.Score;
+import com.example.leaderboardsystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ScoreRepository extends JpaRepository <Score, Long> {
+    List <Score> findByUser (User user);
+    List <Score> findByGameOrderByScoreDesc (String game);
+    List <Score> findTop10ByGameOrderByScoreDesc (String game);
 }
